2aea5f6f09f30fbf6a422a32984beab7a4b8639b
Moonwoven <lavya2713@gmail.com>
2026-03-11 13:21:30 +0530

    Add files via upload

---
+++ FindMatrixSize.java
@@ -0,0 +1,24 @@
+public class FindMatrixSize {
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        System.out.println("Rows = " + rows);
+        System.out.println("Columns = " + cols);
+
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
